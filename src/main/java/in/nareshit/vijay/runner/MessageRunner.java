23e615b4fc99b6a98a9d301dfcf31d7a9d050c0d
vijay547 <vijayreddy547@gmail.com>
2022-05-17 15:14:11 +0530

    WH-996 NEXT COMMIT

--- src/main/java/in/nareshit/vijay/runner/MessageRunner.java
+++ src/main/java/in/nareshit/vijay/runner/MessageRunner.java
@@ -1,15 +1,21 @@
 package in.nareshit.vijay.runner;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 public class MessageRunner implements CommandLineRunner {
+	private static org.slf4j.Logger log= LoggerFactory.getLogger(MessageRunner.class);
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("WELCOME TO SPRING BOOT APP!!");
-		System.out.println("Hello World!");
-		System.out.println("Hello India!");
 		
-	}
-
+		int a =10;
+		try {
+			log.info("STARTED");
+			System.out.println("WELCOME TO SPRING BOOT APP!!+a");
+			} catch (Exception e) {
+				e.printStackTrace();
+		}
+		log.info("END");
+		}
 }
